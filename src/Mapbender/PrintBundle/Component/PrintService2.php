03215428e8faccf5196dee507467340a0635e58e
Christian Wygoda <christian.wygoda@wheregroup.com>
2014-05-30 15:20:12 +0200

    Print: TCPDF-Style vector overlays

--- src/Mapbender/PrintBundle/Component/PrintService2.php
+++ src/Mapbender/PrintBundle/Component/PrintService2.php
@@ -4,6 +4,7 @@ namespace Mapbender\PrintBundle\Component;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use TCPDF_IMPORT as TCPDF;
 use TCPDF_FONTS;
 
@@ -13,8 +14,14 @@ class PrintService2
     protected $container;
     protected $data;
     protected $pdf;
+    protected $tempFiles;
 
-    public function __construct($container)
+    /**
+     * PrintService2 constructor
+     * 
+     * @param ContainerInterface $container Symfony's service container
+     */
+    public function __construct(ContainerInterface $container)
     {
             $this->container = $container;
     }
@@ -24,8 +31,6 @@ class PrintService2
         $this->data = json_decode($content, true);
         $odgParser = new OdgParser($this->container);
         $this->conf = $odgParser->getConf($this->data['template']);
-
-print_r($this->data);die;
 
         $this->createPdf();
         $this->addBackground();
@@ -83,15 +88,25 @@ print_r($this->data);die;
         $finalMapHeight = $mapHeight;
         $finalImageWidth = $imageWidth;
         $finalImageHeight = $imageHeight;
-
+        $scaleX = 1.0;
+        $scaleY = 1.0;
 
         // Extend BBOX and image size to fit rotated AOI
         if(0 != $rotation) {
-            $mapHeight = round(abs(sin(deg2rad($rotation)) * $finalMapHeight) + abs(cos(deg2rad($rotation)) * $finalMapWidth));
-            $mapWidth = round(abs(sin(deg2rad($rotation)) * $finalMapWidth) + abs(cos(deg2rad($rotation)) * $finalMapHeight));
+            // dimensions of a rotated rectangle: http://stackoverflow.com/a/9972699/361488
+            $cr = cos(deg2rad(-$rotation));
+            $cr2 = $cr * $cr;
+            $sr = sin(deg2rad(-$rotation));
+            $sr2 = $sr * $sr;
+            
+            $mapWidth = (1 / ($cr2 - $sr2)) * ($finalMapWidth * $cr - $finalMapHeight * $sr);
+            $mapHeight = (1 / ($cr2 - $sr2)) * (-$finalMapWidth * $sr + $finalMapHeight * $cr);
 
-            $imageWidth = round(abs(sin(deg2rad($rotation)) * $finalImageHeight) + abs(cos(deg2rad($rotation)) * $finalImageWidth));
-            $imageHeight = round(abs(sin(deg2rad($rotation)) * $finalImageWidth) + abs(cos(deg2rad($rotation)) * $finalImageHeight));
+            $imageWidth = (1 / ($cr2 - $sr2)) * ($finalImageWidth * $cr - $finalImageHeight * $sr);
+            $imageHeight = (1 / ($cr2 - $sr2)) * (-$finalImageWidth * $sr + $finalImageHeight * $cr);
+
+            $scaleX= $finalImageWidth / $imageWidth;
+            $scaleY = $finalImageHeight / $imageHeight;
         }
 
         $bbox = array(
@@ -107,44 +122,26 @@ print_r($this->data);die;
             $this->conf['map']['width'] * 10,
             $this->conf['map']['height'] * 10);
 
-        $tempFiles = array();
+        $this->tempFiles = array();
         foreach ($this->data['layers'] as $i => $layer) {
-            // @todo: Is this a safe replacement?
-            $url = strstr($this->data['layers'][$i]['url'], '&BBOX', true);
-            $opacity = $this->data['layers'][$i]['opacity']*100;
-
-            $url = sprintf('%s&BBOX=%s&WIDTH=%d&HEIGHT=%d', $url, implode(',', $bbox), $imageWidth, $imageHeight);
-
-            // @todo:  Replace pattern
-            
-            $this->container->get("logger")->debug("Print Request Nr.: " . $i . ' ' . $url);
-
-            // Get image using our proxy magic
-            $filename = $this->getImageFromURL($url);
-            $tempFiles[] = $filename;
-
-            if(0 != $rotation) {
-                $filename = $this->rotateImage($filename, $rotation, $finalImageWidth, $finalImageHeight);
-                $tempFiles[] = $filename;
+            switch($layer['type']) {
+                case 'wms':
+                    $this->addWmsLayer($layer, $bbox, $imageWidth, $imageHeight, $rotation, $scaleX, $scaleY, $mapBox);
+                    break;
+                case 'GeoJSON+Style':
+                    $this->addFeatureLayer($layer, $bbox, $rotation, $scaleX, $scaleY, $mapBox);
+                    break;
+                default:
+                    throw new RuntimeException('Unhandled layer type ' . $layer['type']);
             }
-
-            // @todo: Use real layer name
-            $this->pdf->startLayer('Layer ' . $i, true, true, false);
-            $this->pdf->setAlpha(1, 'Normal', $this->data['layers'][$i]['opacity']);
-            $this->pdf->image(
-                $filename,
-                $mapBox[0],
-                $mapBox[1],
-                $mapBox[2],
-                $mapBox[3]);
-            $this->pdf->endLayer();
         }
 
         // Be nice an clean up
         $this->pdf->setAlpha(1, 'Normal', 1);
-        foreach($tempFiles as $tempFile) {
+        foreach($this->tempFiles as $tempFile) {
             unlink($tempFile);
         }
+        $this->tempFiles = array();
 
         call_user_func_array(array(&$this->pdf, 'rect'), $mapBox);
     }
@@ -230,6 +227,159 @@ print_r($this->data);die;
             $this->pdf->output('test.pdf', $mode ? $mode : 'D');
     }
 
+    protected function addWmsLayer($layer, $bbox, $imageWidth, $imageHeight, $rotation, $scaleX, $scaleY, $mapBox)
+    {
+            // @todo: Is this a safe replacement?
+            $url = strstr($layer['url'], '&BBOX', true);
+
+            $url = sprintf('%s&BBOX=%s&WIDTH=%d&HEIGHT=%d', $url, implode(',', $bbox), $imageWidth, $imageHeight);
+
+            // @todo:  Replace pattern
+            
+            // Get image using our proxy magic
+            $filename = $this->getImageFromURL($url);
+            $this->tempFiles[] = $filename;
+
+            if(0 != $rotation) {
+                $filename = $this->rotateImage($filename, $rotation, $scaleX, $scaleY);
+                $this->tempFiles[] = $filename;
+            }
+
+            // @todo: Use real layer name
+            $this->pdf->startLayer('Layer', true, true, false);
+            $this->pdf->setAlpha(1, 'Normal', $layer['opacity']);
+            $this->pdf->image(
+                $filename,
+                $mapBox[0],
+                $mapBox[1],
+                $mapBox[2],
+                $mapBox[3]);
+            $this->pdf->endLayer();
+    }
+
+    protected function addFeatureLayer($layer, $bbox, $rotation, $scaleX, $scaleY, $mapBox)
+    {
+        $transformer = function($points) use ($mapBox, $bbox, $rotation, $scaleX, $scaleY) {
+            array_walk($points, function(&$coords) use ($mapBox, $bbox, $rotation, $scaleX, $scaleY) {
+                $x = $coords[0];
+                $y = $coords[1];
+                // Calculate scale and translation parameters
+                $sx = $mapBox[2] / ($bbox[2] - $bbox[0]) / $scaleX;
+                $sy = $mapBox[3] / ($bbox[3] - $bbox[1]) / $scaleY;
+                $tx = ($bbox[0] + $bbox[2]) / 2;
+                $ty = ($bbox[1] + $bbox[3]) / 2;
+
+                // Do transform
+                $a = ($x - $tx) * $sx;
+                $b = ($ty - $y) * $sy;
+
+                if(0 != $rotation) {
+                    $x = deg2rad(-$rotation);
+                    $cx = cos($x);
+                    $sx = sin($x);
+                    $a_ = $a;
+                    $b_ = $b;
+
+                    $a = $cx * $a_ - $sx * $b_;
+                    $b = $sx * $a_ + $cx * $b_;
+                }
+
+                // Finally, shift to map box
+                $coords[0] = $a + $mapBox[0] + $mapBox[2] / 2;
+                $coords[1] = $b + $mapBox[1] + $mapBox[3] / 2;
+            });
+            return $points;
+        };
+
+        // Draw features
+        foreach($layer['geometries'] as $geometry) {
+            $renderMethodName = 'render' . $geometry['type'];
+            if(!method_exists($this, $renderMethodName)) {
+                throw new \RuntimeException('Can not draw geometries of type "' . $geometry['type'] . '".');
+            }
+
+            $this->$renderMethodName($geometry, $transformer);
+        }
+    }
+
+    protected function renderPoint($geometry, $transformer)
+    {
+        /*
+            [fillColor] => #ee9900
+            [fillOpacity] => 0.4
+            [strokeColor] => #ee9900
+            [strokeOpacity] => 1
+            [strokeWidth] => 1
+            [strokeLinecap] => round
+            [strokeDashstyle] => solid
+            [pointRadius] => 6
+        */
+        $this->pdf->SetLineStyle(array('width' => 0.5, 'cap' => 'butt', 'join' => 'miter', 'dash' => 0, 'color' => array(0, 0, 0)));
+
+        $coords = $transformer(array($geometry['coordinates']));
+        $this->pdf->circle($coords[0][0], $coords[0][1], 5, 0, 360, 'DF');
+    }
+
+    private function renderLineString($geometry, $transformer)
+    {
+        /*
+            [strokeColor] => #ee9900
+            [strokeOpacity] => 1
+            [strokeWidth] => 1
+            [strokeLinecap] => round
+            [strokeDashstyle] => solid
+         */
+        $points = $transformer($geometry['coordinates']);
+        $lineStyle = $this->getLineStyle($geometry['style']);
+        $preAlpha = $this->pdf->getAlpha();
+        $this->pdf->setAlpha($geometry['style']['strokeOpacity'], 'Normal', $geometry['style']['strokeOpacity'], true);
+        $this->pdf->polyline($this->flattenPointsArray($points), 'S', array('all' => $lineStyle));
+        //array(4) { ["CA"]=> float(1) ["ca"]=> float(0.85) ["BM"]=> string(7) "/Normal" ["AIS"]=> bool(false) }
+    }
+
+    protected function renderPolygon($geometry, $transformer)
+    {
+            /*
+            [fillColor] => #ee9900
+            [fillOpacity] => 0.4
+            [strokeColor] => #ee9900
+            [strokeOpacity] => 1
+            [strokeWidth] => 1
+            [strokeLinecap] => round
+            [strokeDashstyle] => solid
+        */
+        foreach($geometry['coordinates'] as $ring) {
+            if(count($ring) < 3) {
+                continue;
+            }
+
+            
+            $points = $transformer($ring);
+            $this->pdf->polygon($this->flattenPointsArray($points), 'DF');
+        }
+    }
+
+    protected function flattenPointsArray($points) {
+        $coords = array();
+        array_walk_recursive($points, function($coord) use (&$coords) {
+            $coords[] = $coord;
+        });
+
+        return $coords;
+    }
+
+    protected function getLineStyle(array $style)
+    {
+        return array(
+            'width' => 5* $style['strokeWidth'],
+            'cap' => $style['strokeLinecap'],
+            'join' => 'round',
+            'dash' => 0,  // @todo: parse strokeDashstyle
+            'phase' => 0,
+            'color' => CSSColorParser::parse($style['strokeColor'])
+        );
+    }
+
     protected function getImageFromURL($url)
     {
         $attributes = array();
@@ -262,7 +412,7 @@ print_r($this->data);die;
         return $filename;
     }
 
-    protected function rotateImage($filename, $rotation, $width, $height)
+    protected function rotateImage($filename, $rotation, $scaleX, $scaleY)
     {
         // Load image
         $size = getimagesize($filename);
@@ -278,7 +428,12 @@ print_r($this->data);die;
         $transColor = imagecolorallocatealpha($image, 255, 255, 255, 127);
         $rotatedImage = imagerotate($image, $rotation, $transColor);
 
+        //header('Content-Type: image/png');
+        //imagepng($image);
+
         // Clip image        
+        $width = round(imagesx($image) * $scaleX);
+        $height = round(imagesy($image) * $scaleY);
         $clippedImage = imagecreatetruecolor($width, $height);
         imagealphablending($clippedImage, false);
         imagesavealpha($clippedImage, true);
