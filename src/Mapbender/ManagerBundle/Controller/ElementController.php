2e5be1cf688f0e92d5404435d7e8c7ca85f93871
Andreas Schmitz <andreas.schmitz@wheregroup.com>
2015-07-01 16:48:35 +0200

    fixed missing import

--- src/Mapbender/ManagerBundle/Controller/ElementController.php
+++ src/Mapbender/ManagerBundle/Controller/ElementController.php
@@ -11,6 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FOM\ManagerBundle\Configuration\Route as ManagerRoute;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Mapbender\CoreBundle\Component\Element As ComponentElement;
