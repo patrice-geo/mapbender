21b402380c43b7896dcd703f3ac9e18e7b90e3f1
Tomy Keller <tomy.keller@wheregroup.com>
2015-07-10 14:33:59 +0200

    remove scaledisplay element from CoreBundle

--- src/Mapbender/CoreBundle/MapbenderCoreBundle.php
+++ src/Mapbender/CoreBundle/MapbenderCoreBundle.php
@@ -62,7 +62,7 @@ class MapbenderCoreBundle extends MapbenderBundle
             'Mapbender\CoreBundle\Element\PrintClient',
             #'Mapbender\CoreBundle\Element\Ruler',
             #'Mapbender\CoreBundle\Element\ScaleBar',
-            'Mapbender\CoreBundle\Element\ScaleDisplay',
+            #'Mapbender\CoreBundle\Element\ScaleDisplay',
             'Mapbender\CoreBundle\Element\ScaleSelector',
             'Mapbender\CoreBundle\Element\SearchRouter',
             'Mapbender\CoreBundle\Element\SimpleSearch',
