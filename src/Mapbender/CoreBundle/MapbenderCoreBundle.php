a39ac30a48b1716741273393c54fe00dd84f9b26
Tomy Keller <tomy.keller@wheregroup.com>
2015-07-13 14:16:26 +0200

    remove Sketch element from CoreBundle

--- src/Mapbender/CoreBundle/MapbenderCoreBundle.php
+++ src/Mapbender/CoreBundle/MapbenderCoreBundle.php
@@ -66,7 +66,7 @@ class MapbenderCoreBundle extends MapbenderBundle
             #'Mapbender\CoreBundle\Element\ScaleSelector',
             #'Mapbender\CoreBundle\Element\SearchRouter',
             'Mapbender\CoreBundle\Element\SimpleSearch',
-            'Mapbender\CoreBundle\Element\Sketch',
+            #'Mapbender\CoreBundle\Element\Sketch',
             #'Mapbender\SrsSelectorBundle\Element\SrsSelector',
             #'Mapbender\CoreBundle\Element\ZoomBar',
             'Mapbender\CoreBundle\Element\Redlining'
