1f25700cccec4f283f726cd5f9ecfc9ea3820486
Tomy Keller <tomy.keller@wheregroup.com>
2015-07-13 13:48:36 +0200

    add SearchRouterBundle

--- src/Mapbender/CoreBundle/MapbenderCoreBundle.php
+++ src/Mapbender/CoreBundle/MapbenderCoreBundle.php
@@ -64,7 +64,7 @@ class MapbenderCoreBundle extends MapbenderBundle
             #'Mapbender\CoreBundle\Element\ScaleBar',
             #'Mapbender\CoreBundle\Element\ScaleDisplay',
             #'Mapbender\CoreBundle\Element\ScaleSelector',
-            'Mapbender\CoreBundle\Element\SearchRouter',
+            #'Mapbender\CoreBundle\Element\SearchRouter',
             'Mapbender\CoreBundle\Element\SimpleSearch',
             'Mapbender\CoreBundle\Element\Sketch',
             #'Mapbender\SrsSelectorBundle\Element\SrsSelector',
