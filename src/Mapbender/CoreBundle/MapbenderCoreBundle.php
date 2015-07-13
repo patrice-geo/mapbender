c3a84add2b7af67cbb11d5fc57d6eb25688cc021
Tomy Keller <tomy.keller@wheregroup.com>
2015-07-13 16:03:23 +0200

    remove simpleSearch element from CoreBundle

--- src/Mapbender/CoreBundle/MapbenderCoreBundle.php
+++ src/Mapbender/CoreBundle/MapbenderCoreBundle.php
@@ -65,7 +65,7 @@ class MapbenderCoreBundle extends MapbenderBundle
             #'Mapbender\CoreBundle\Element\ScaleDisplay',
             #'Mapbender\CoreBundle\Element\ScaleSelector',
             #'Mapbender\CoreBundle\Element\SearchRouter',
-            'Mapbender\CoreBundle\Element\SimpleSearch',
+            #'Mapbender\CoreBundle\Element\SimpleSearch',
             #'Mapbender\CoreBundle\Element\Sketch',
             #'Mapbender\SrsSelectorBundle\Element\SrsSelector',
             #'Mapbender\CoreBundle\Element\ZoomBar',
