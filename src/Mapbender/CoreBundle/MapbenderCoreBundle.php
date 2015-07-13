6daa90849c39d0e3a4082c225cb334f653b5761c
Tomy Keller <tomy.keller@wheregroup.com>
2015-07-13 11:24:06 +0200

    remove zoomBar element from CoreBundle

--- src/Mapbender/CoreBundle/MapbenderCoreBundle.php
+++ src/Mapbender/CoreBundle/MapbenderCoreBundle.php
@@ -68,7 +68,7 @@ class MapbenderCoreBundle extends MapbenderBundle
             'Mapbender\CoreBundle\Element\SimpleSearch',
             'Mapbender\CoreBundle\Element\Sketch',
             #'Mapbender\SrsSelectorBundle\Element\SrsSelector',
-            'Mapbender\CoreBundle\Element\ZoomBar',
+            #'Mapbender\CoreBundle\Element\ZoomBar',
             'Mapbender\CoreBundle\Element\Redlining'
         );
     }
