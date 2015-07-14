e33bf92306ebd957f484a45d2a44a4ddd90d5fa9
Tomy Keller <tomy.keller@wheregroup.com>
2015-07-14 14:42:21 +0200

    rm layertree element from CoreBundle

--- src/Mapbender/CoreBundle/MapbenderCoreBundle.php
+++ src/Mapbender/CoreBundle/MapbenderCoreBundle.php
@@ -54,7 +54,7 @@ class MapbenderCoreBundle extends MapbenderBundle
             #'Mapbender\CoreBundle\Element\FeatureInfo',
             #'Mapbender\CoreBundle\Element\GpsPosition',
             #'Mapbender\CoreBundle\Element\HTMLElement',
-            'Mapbender\CoreBundle\Element\Layertree',
+            #'Mapbender\CoreBundle\Element\Layertree',
             #'Mapbender\CoreBundle\Element\Legend',
             'Mapbender\CoreBundle\Element\Map',
             #'Mapbender\CoreBundle\Element\Overview',
